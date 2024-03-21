70e196b2f933a7dfd791d793469b064db45d6aa4
juanRCoder <guillecarck@gmail.com>
2024-03-21 16:34:49 -0500

    feat: add new project and new color primary

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -11,10 +11,10 @@ import {
 } from "react-icons/si";
 import { DiJqueryLogo } from "react-icons/di";
 import project_HVA from "../assets/project_HVA.png";
-import project_BS from "../assets/project_BS.png";
 import projet_EBOOK from "../assets/project_EBOOK.png";
 import project_ST from "../assets/project_ST.png";
 import project_NP from "../assets/project_NotePad.png";
+import project_TQ from "../assets/project_TQ.png";
 
 function Proyectos() {
   return (
@@ -81,38 +81,43 @@ function Proyectos() {
         <div className="boxProject">
           <div className="boxImagen">
             <div className="boxImagen_img">
-              <img src={project_BS} title="HVA" alt="HVA" />
+              <img src={project_TQ} title="themeQuizz" alt="themeQuizz" />
             </div>
           </div>
           <div className="boxProjectInfo">
-            <h1>Bank Simulator</h1>
+            <h1>Theme Quizz</h1>
             <div className="boxTecnologias">
               <button>
-                <SiMongodb className="icon mongo" />
-                <p>MongoDB</p>
+                <SiAngular className="icon angular" />
+                <p>angular</p>
               </button>
               <button>
-                <SiExpress className="icon express" />
-                <p>ExpressJs</p>
+                <SiTypescript className="icon typescript" />
+                <p>typescript</p>
               </button>
               <button>
-                <FaReact className="icon react" />
-                <p>React</p>
-              </button>
-              <button>
-                <FaNodeJs className="icon" />
-                <p>NodeJs</p>
+                <FaSass className="icon scss" />
+                <p>scss</p>
               </button>
             </div>
             <p className="infoProject">
-              Aplicación web que permite retirar/depositar dinero como un agente
-              bancario. También facilita al usuario ingresar y consultar sus
-              movimientos e información financiera en tiempo real.
+              Desafía tu conocimiento con una aplicación que presenta una serie de
+              preguntas divididas por dificultad y cuatro categorías principales. Aprende 
+              algo nuevo cada dia diviertiendo en la aplicación.
             </p>
             <div className="linksProject">
+              <button className="web">
+                <a
+                  href="https://theme-quiz.netlify.app/"
+                  target="_BLANK"
+                >
+                  <p>WebSite</p>
+                  <TfiWorld />
+                </a>
+              </button>
               <button className="github">
                 <a
-                  href="https://github.com/juanRCoder/Bank-Simulator"
+                  href="https://github.com/juanRCoder/Theme_Quizz"
                   target="_BLANK"
                 >
                   <p>GitHub</p>
@@ -247,12 +252,15 @@ function Proyectos() {
             </div>
             <p className="infoProject">
               Gestor de notas el cual realiza las operaciones basicas de gestión
-              crear, leer, actualizar, eliminar notas en este caso, tambien tiene
-              una autenticación de usuarios para mayor seguridad.
+              crear, leer, actualizar, eliminar notas en este caso, tambien
+              tiene una autenticación de usuarios para mayor seguridad.
             </p>
             <div className="linksProject">
               <button className="web">
-                <a href="https://www.youtube.com/watch?v=FytfIC_IsOY" target="_BLANK">
+                <a
+                  href="https://www.youtube.com/watch?v=FytfIC_IsOY"
+                  target="_BLANK"
+                >
                   <p>Video-Demo</p>
                   <TfiWorld />
                 </a>
@@ -269,6 +277,7 @@ function Proyectos() {
             </div>
           </div>
         </div>
+   
       </div>
     </section>
   );
