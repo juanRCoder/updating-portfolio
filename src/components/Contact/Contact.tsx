325e7efde790c8e5f21b1f7c1f381365b59d53fa
juanRCoder <guillecarck@gmail.com>
2024-07-09 15:46:10 -0500

    update: email only for jobjs

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,7 +9,7 @@ function Contact() {
         <p>¡Estoy listo para comenzar a trabajar en tu proyecto! Contáctame para coordinar y empezar a trabajar en equipo.</p>
       </div>
       <div className='aboutMe'>
-        <a className='a' href="mailto:guillecarck@gmail.com">
+        <a className='a' href="mailto:juanRamirezMontalvan@gmail.com">
           <CgMail className='img'/>
           <p className='texto'>guillecarck</p>
         </a>
